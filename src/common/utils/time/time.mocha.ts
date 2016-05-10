da1737a3b9cc8dee77b32672ab063875147c2692
eva <liou.eva@gmail.com>
2016-05-09 17:41:48 -0700

    no client dependency

--- src/common/utils/time/time.mocha.ts
+++ src/common/utils/time/time.mocha.ts
@@ -2,7 +2,6 @@ import { expect } from 'chai';
 import { Timezone, Duration, day, month } from 'chronoshift';
 import { TimeRange } from 'plywood';
 import { datesEqual, prependDays, appendDays, getEndWallTimeInclusive, getWallTimeDay, getWallTimeMonthWithYear, formatTimeBasedOnGranularity, formatTimeRange } from './time';
-import { getLocale } from "../../../client/config/constants";
 
 var { WallTime } = require('chronoshift');
 if (!WallTime.rules) {
@@ -97,31 +96,38 @@ describe('Time', () => {
   });
 
   it('formats time range based off of start walltime', () => {
+
+    var locale = {
+      shortDays: ["2"],
+      weekStart: 0,
+      shortMonths: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" ]
+    };
+
     var start = new Date("1965-02-02T13:00:00.000Z");
     var end = day.shift(start, TZ_TIJUANA, 1);
     var gran = Duration.fromJS('PT1H');
     var range = new TimeRange({ start, end });
-    expect(formatTimeBasedOnGranularity(range, gran, TZ_TIJUANA, getLocale()), 'hour tijuana').to.equal("Feb 2, 1965, 5am");
+    expect(formatTimeBasedOnGranularity(range, gran, TZ_TIJUANA, locale), 'hour tijuana').to.equal("Feb 2, 1965, 5am");
 
     start = new Date("1999-05-02T13:00:00.000Z");
     end = month.shift(start, TZ_TIJUANA, 1);
     gran = Duration.fromJS('PT1S');
     range = new TimeRange({ start, end });
-    expect(formatTimeBasedOnGranularity(range, gran, TZ_TIJUANA, getLocale()), 'second tijuana').to.equal("May 2, 06:00:00");
+    expect(formatTimeBasedOnGranularity(range, gran, TZ_TIJUANA, locale), 'second tijuana').to.equal("May 2, 06:00:00");
 
     start = new Date("1999-05-02T13:00:00.000Z");
     end = month.shift(start, TZ_TIJUANA, 1);
     gran = Duration.fromJS('P1W');
     range = new TimeRange({ start, end });
-    expect(formatTimeBasedOnGranularity(range, gran, TZ_TIJUANA, getLocale()), 'week tijuana').to.equal("May 2 - Jun 2, 1999 6am");
+    expect(formatTimeBasedOnGranularity(range, gran, TZ_TIJUANA, locale), 'week tijuana').to.equal("May 2 - Jun 2, 1999 6am");
 
     start = new Date("1999-05-02T13:00:00.000Z");
     end = month.shift(start, TZ_KATHMANDU, 1);
     gran = Duration.fromJS('P1M');
     range = new TimeRange({ start, end });
-    var monthFmt = formatTimeBasedOnGranularity(range, gran, TZ_KATHMANDU, getLocale());
+    var monthFmt = formatTimeBasedOnGranularity(range, gran, TZ_KATHMANDU, locale);
     expect(monthFmt, 'month granularity format').to.equal("May, 1999");
-    var minFmt = formatTimeBasedOnGranularity(range, Duration.fromJS("PT1M"), TZ_KATHMANDU, getLocale());
+    var minFmt = formatTimeBasedOnGranularity(range, Duration.fromJS("PT1M"), TZ_KATHMANDU, locale);
     expect(minFmt, 'minute granularity format').to.equal("May 2, 6:45pm");
     expect(monthFmt).to.not.equal(minFmt, 'distinguishes between month and minute fmt');
 
