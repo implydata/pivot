ee46109dd427237cdfee7e3dcac476c854e64e73
eva <eva@Evas-MBP.attlocal.net>
2016-06-15 13:45:02 -0700

    feedback fixes

--- src/client/visualizations/base-visualization/base-visualization.tsx
+++ src/client/visualizations/base-visualization/base-visualization.tsx
@@ -123,11 +123,10 @@ export class BaseVisualization<S extends BaseVisualizationState>
       } else if (limitAction) {
         subQuery = subQuery.performAction(limitAction);
       } else if (splitDimension.kind === 'number') {
-      // Plywood converts groupBys to topN if the limit is below a certain threshold.  Currently sorting on dimension in a groupBy query does not
+      // Hack: Plywood converts groupBys to topN if the limit is below a certain threshold.  Currently sorting on dimension in a groupBy query does not
       // behave as expected and in the future plywood will handle this, but for now add a limit so a topN query is performed.
       // 5000 is just a randomly selected number that's high enough that it's not immediately obvious that there's a limit.
-        var highLimitAction = new LimitAction({ limit: 5000 });
-        subQuery = subQuery.performAction(highLimitAction);
+        subQuery = subQuery.limit(5000);
       }
 
       if (i + 1 < splits.length()) {
